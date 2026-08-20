f9d00253e3a33a8d742c67795c909fe88f598b84
Adryc Allen Catapang <catapangadrycallen@gmail.com>
2026-08-21 05:13:22 +0800

    wip

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -2,18 +2,23 @@
 {{flutter_build_config}}
 
 const userAgent = navigator.userAgent || "";
-const isMetaInAppBrowser = /FBAN|FBAV|Messenger|Instagram/i.test(userAgent);
+const isSocialInAppBrowser =
+  /FBAN|FBAV|Messenger|Instagram|Line\/|Line |MicroMessenger|WeChat|Viber|Telegram|Twitter|X\/|Snapchat|LinkedInApp/i.test(
+    userAgent,
+  );
 
-_flutter.loader.load({
-  config: {
-    canvasKitBaseUrl: "canvaskit/",
-  },
-  onEntrypointLoaded: async function (engineInitializer) {
-    const appRunner = await engineInitializer.initializeEngine();
-    await appRunner.runApp();
-  },
-});
+window.__AATAPPP_SOCIAL_INAPP__ = isSocialInAppBrowser;
 
-if (isMetaInAppBrowser) {
-  document.documentElement.setAttribute("data-meta-inapp", "true");
+if (isSocialInAppBrowser) {
+  document.documentElement.setAttribute("data-social-inapp", "true");
+} else {
+  _flutter.loader.load({
+    config: {
+      canvasKitBaseUrl: "canvaskit/",
+    },
+    onEntrypointLoaded: async function (engineInitializer) {
+      const appRunner = await engineInitializer.initializeEngine();
+      await appRunner.runApp();
+    },
+  });
 }
